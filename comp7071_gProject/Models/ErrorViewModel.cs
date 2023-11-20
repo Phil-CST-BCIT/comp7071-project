ed0949f39542ed5ddca598f78d299026a6417e5d
BeeWang015 <bkwang015@outlook.com>
2023-11-19 22:38:02 -0800

    removing excess folders and files

--- comp7071_gProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace comp7071_gProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
