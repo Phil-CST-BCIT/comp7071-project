0e9f0662e8cd52e1cce80ca10cc68fcfc556346c
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:06:37 -0800

    fix relationships

--- comp7071_project/Models/LineItem.cs
+++ comp7071_project/Models/LineItem.cs
@@ -5,13 +5,11 @@ namespace comp7071_project.Models;
 
 public partial class LineItem
 {
-    public int ServiceId { get; set; }
+    public int Id { get; set; }
+
+    public int Hours { get; set; }
 
     public int InvoiceId { get; set; }
 
-    public int? Hours { get; set; }
-
-    public virtual Invoice Invoice { get; set; } = null!;
-
-    public virtual Service Service { get; set; } = null!;
+    public int ServiceId { get; set; }
 }
