051e065af5acee44f0c79ca4aeabb422ca6789bf
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:05:08 -0800

    make nonnullable

--- comp7071_project/Models/Renter.cs
+++ comp7071_project/Models/Renter.cs
@@ -7,7 +7,7 @@ public partial class Renter
 {
     public int Id { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     public string? EmergencyContactEmail { get; set; }
 
