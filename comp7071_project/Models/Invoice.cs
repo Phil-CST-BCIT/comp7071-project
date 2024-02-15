55c0096d2c609e34281c668a73bbb860f116a018
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:06:55 -0800

    make nonnullable and fix relationships

--- comp7071_project/Models/Invoice.cs
+++ comp7071_project/Models/Invoice.cs
@@ -7,13 +7,14 @@ public partial class Invoice
 {
     public int Id { get; set; }
 
-    public double? RentAmount { get; set; }
+    public double PaymentAmount { get; set; }
 
-    public double? Tax { get; set; }
+    public double Tax { get; set; }
 
-    public DateOnly? DueDate { get; set; }
+    public DateOnly DueDate { get; set; }
 
+    public int RenterID { get; set; }
     public virtual Renter Renter { get; set; }
     
-    public virtual ICollection<LineItem> LineItems { get; set; } = new List<LineItem>();
+    public List<LineItem> LineItems { get; } = new List<LineItem>();
 }
