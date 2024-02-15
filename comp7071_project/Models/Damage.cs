0b7b52dab63b2e491e117c3cbc4ed88f4219e187
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:03:19 -0800

    make nonnullable, and group relationship attrs

--- comp7071_project/Models/Damage.cs
+++ comp7071_project/Models/Damage.cs
@@ -6,12 +6,11 @@ namespace comp7071_project.Models;
 public partial class Damage
 {
     public int Id { get; set; }
+    
+    public string Description { get; set; }
+
+    public DateOnly DateReported { get; set; }
 
     public int AssetId { get; set; }
-
-    public string? Description { get; set; }
-
-    public DateOnly? DateReported { get; set; }
-
     public virtual Asset Asset { get; set; } = null!;
 }
