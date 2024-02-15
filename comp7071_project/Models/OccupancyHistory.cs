d52a6a15bf7d6f6ea892fbf431736972bbd5a57b
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:04:25 -0800

    make nonnullable and group relationship attrs

--- comp7071_project/Models/OccupancyHistory.cs
+++ comp7071_project/Models/OccupancyHistory.cs
@@ -7,13 +7,12 @@ public partial class OccupancyHistory
 {
     public int Id { get; set; }
 
-    public int AssetId { get; set; }
-
-    public DateOnly? StartDate { get; set; }
+    public DateOnly StartDate { get; set; }
 
     public DateOnly? EndDate { get; set; }
 
     public string? LicensePlate { get; set; }
 
+    public int AssetId { get; set; }
     public virtual Asset Asset { get; set; } = null!;
 }
