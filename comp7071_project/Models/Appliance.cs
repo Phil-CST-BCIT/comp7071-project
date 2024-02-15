55518aa6decbd69340a76109dc9fd118b58e58c0
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:01:27 -0800

    make nonnullable and group relationship attrs

--- comp7071_project/Models/Appliance.cs
+++ comp7071_project/Models/Appliance.cs
@@ -6,14 +6,11 @@ namespace comp7071_project.Models;
 public partial class Appliance
 {
     public int Id { get; set; }
+    
+    public string Make { get; set; }
+
+    public string Model { get; set; }
 
     public int AssetId { get; set; }
-
-    public string? Make { get; set; }
-
-    public string? Model { get; set; }
-
     public virtual Asset Asset { get; set; } = null!;
-
-    public virtual ICollection<MaintenanceRequest> MaintenanceRequests { get; set; } = new List<MaintenanceRequest>();
 }
