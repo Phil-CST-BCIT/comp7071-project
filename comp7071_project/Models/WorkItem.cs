8c9267f78163f4932f06e2308eab7bdef3faa01f
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:08:11 -0800

    comment out

--- comp7071_project/Models/WorkItem.cs
+++ comp7071_project/Models/WorkItem.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace comp7071_project.Models;
-
-public partial class WorkItem
-{
-    public int Id { get; set; }
-
-    public int MaintenanceRequestId { get; set; }
-
-    public string? Status { get; set; }
-
-    public virtual MaintenanceRequest MaintenanceRequest { get; set; } = null!;
-}
+﻿// using System;
+// using System.Collections.Generic;
+//
+// namespace comp7071_project.Models;
+//
+//
+// public partial class WorkItem
+// {
+//     public int Id { get; set; }
+//
+//     public int MaintenanceRequestId { get; set; }
+//
+//     public virtual MaintenanceRequest MaintenanceRequest { get; set; } = null!;
+// }
