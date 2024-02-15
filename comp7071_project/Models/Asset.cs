26808ab5139d98dbbe19c7bdd5c243ac9b64dc82
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:02:46 -0800

    make nonnullable, make status enum, and group relationship attrs

--- comp7071_project/Models/Asset.cs
+++ comp7071_project/Models/Asset.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 
 namespace comp7071_project.Models;
 
+public enum AssetStatus
+{
+    Inventory,
+    Deployed,
+    WrittenOff
+}
+
 public partial class Asset
 {
     public int Id { get; set; }
 
-    public string? Type { get; set; }
+    public string Type { get; set; }
 
-    public string? Status { get; set; }
+    public AssetStatus Status { get; set; }
 
-    public string? Location { get; set; }
+    public string Location { get; set; }
 
     public virtual ICollection<Appliance> Appliances { get; set; } = new List<Appliance>();
-
-    public virtual Application? Application { get; set; }
-
     public virtual ICollection<Damage> Damages { get; set; } = new List<Damage>();
 
     public virtual ICollection<OccupancyHistory> OccupancyHistories { get; set; } = new List<OccupancyHistory>();
