f63cde7b0929a7897922188148d832a7c7f68cb2
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:06:03 -0800

    make nonnullable and make list not collection

--- comp7071_project/Models/Service.cs
+++ comp7071_project/Models/Service.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace comp7071_project.Models;
+using comp7071_project.Models;
 
 public partial class Service
 {
     public int Id { get; set; }
 
-    public string? Type { get; set; }
+    public string Type { get; set; }
 
-    public double? Rate { get; set; }
+    public double Rate { get; set; }
 
-    public virtual ICollection<LineItem> LineItems { get; set; } = new List<LineItem>();
+    public List<LineItem> LineItems { get; } = new List<LineItem>();
 }
