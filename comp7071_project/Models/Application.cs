5b251eed5b24a37e2a3a7287935e242e816e6dca
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:02:00 -0800

    group relationship attrs

--- comp7071_project/Models/Application.cs
+++ comp7071_project/Models/Application.cs
@@ -5,11 +5,11 @@ namespace comp7071_project.Models;
 
 public partial class Application
 {
+    public int Id { get; set; }
+   
     public int ApplicantId { get; set; }
-
-    public int AssetId { get; set; }
-
     public virtual Applicant Applicant { get; set; } = null!;
 
+    public int AssetId { get; set; }
     public virtual Asset Asset { get; set; } = null!;
 }
