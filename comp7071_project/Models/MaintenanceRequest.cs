94dda39adf4348ecac1fc635f2fd7a4b9e2cc83a
bakedSpaceTime <tushyais@gmail.com>
2024-02-14 23:07:39 -0800

    make nonnullable, make status enum, and group relationship attrs

--- comp7071_project/Models/MaintenanceRequest.cs
+++ comp7071_project/Models/MaintenanceRequest.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 
 namespace comp7071_project.Models;
 
+public enum MaintenanceRequestStatus
+{
+    New,
+    InProgress,
+    Complete,
+    Declined
+}
+
 public partial class MaintenanceRequest
 {
     public int Id { get; set; }
+    
+    public DateOnly Date { get; set; }
+
+    public string Description { get; set; }
+
+    public MaintenanceRequestStatus Status { get; set; }
+
+    public int ApplianceId { get; set; }
+    public virtual Appliance Appliance { get; set; }
 
     public int RenterId { get; set; }
-
-    public int? ApplianceId { get; set; }
-
-    public DateOnly? Date { get; set; }
-
-    public string? Description { get; set; }
-
-    public virtual Appliance? Appliance { get; set; }
-
-    public virtual ICollection<JobReport> JobReports { get; set; } = new List<JobReport>();
-
     public virtual Renter Renter { get; set; } = null!;
 
-    public virtual ICollection<WorkItem> WorkItems { get; set; } = new List<WorkItem>();
+    public virtual ICollection<JobReport> JobReports { get; set; } = new List<JobReport>();
 }
