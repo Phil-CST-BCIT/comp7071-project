f5d1056f0716a446a75dee01f82ef2fe36e40879
bakedSpaceTime <tushyais@gmail.com>
2024-02-20 21:17:49 -0800

    add triggers to EF definitions

--- comp7071_project/Models/Comp7071ProjectContext.cs
+++ comp7071_project/Models/Comp7071ProjectContext.cs
@@ -56,6 +56,21 @@ public partial class Comp7071ProjectContext : DbContext
     public virtual DbSet<InvoiceFact> InvoiceFact { get; set; }
     
     public virtual DbSet<RentHistoryDim> RentHistoryDim { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Need to inform EF that the table associated has a trigger in place
+        // https://learn.microsoft.com/en-us/ef/core/providers/sql-server/misc
+        modelBuilder.Entity<Asset>()
+            .ToTable(tb => tb.HasTrigger("AfterApplianceINSERT"));
+        modelBuilder.Entity<Asset>()
+            .ToTable(tb => tb.HasTrigger("AfterAssetINSERT"));
+        modelBuilder.Entity<Asset>()
+            .ToTable(tb => tb.HasTrigger("AfterRenterINSERT"));
+        modelBuilder.Entity<Asset>()
+            .ToTable(tb => tb.HasTrigger("AfterMaintenanceRequestINSERT"));
+        modelBuilder.Entity<Asset>()
+            .ToTable(tb => tb.HasTrigger("AfterInvoiceINSERT"));
+    }
 //
 //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
