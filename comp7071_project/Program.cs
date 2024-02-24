a4d384922f89f8154d6b31bc72c128bde2739642
bakedSpaceTime <tushyais@gmail.com>
2024-02-23 20:41:28 -0800

    db context builder

--- comp7071_project/Program.cs
+++ comp7071_project/Program.cs
@@ -1,6 +1,11 @@
+using comp7071_project.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Comp7071ProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Comp7071ProjectContext") ?? throw new InvalidOperationException("Connection string 'Comp7071ProjectContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,7 +13,6 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
